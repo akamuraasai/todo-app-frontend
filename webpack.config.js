098dc31164effc4ff00a67b8310e0b43096a2bd7
Jonathan Willian <jonathan.willian.tod@gmail.com>
2017-04-01 03:09:59 -0300

    Commit inicial

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+const ExtTxtPg = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: __dirname + '/public',
+        filename: './app.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public',
+    },
+    resolve: {
+        extension: ['', '.js', '.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules'
+        }
+    },
+    plugins: [
+        new ExtTxtPg('app.css')
+    ],
+    modules: {
+        loaders: [{
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
+            }
+        }, {
+            test: /\.css$/,
+            loader: ExtTxtPg.extract('style-loader', 'css-loader')
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+            loader: 'file'
+        }]
+    }
+}
