6f42d4d5aa5e35a721966b5bc6714bbf33bd9ea8
Jonathan Willian <jonathan.willian.tod@gmail.com>
2017-04-01 04:06:26 -0300

    Criado o page header

--- src/sobre/sobre.jsx
+++ src/sobre/sobre.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import PageHeader from '../template/pageHeader';
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader titulo="Sobre" subtitulo="Nós"/>
+
+        <h2>Nossa Historia</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet corporis delectus dignissimos eos excepturi illo minima mollitia numquam optio quo? Corporis, dolorem earum id labore quasi quibusdam quidem soluta vel!</p>
+
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deleniti eius ipsam.</p>
+
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum illo quae quasi quisquam quod, similique.</p>
     </div>
 );
