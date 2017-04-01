10f6237e06fb39a6370d585ff9851424b19cf1a3
Jonathan Willian <jonathan.willian.tod@gmail.com>
2017-04-01 06:10:18 -0300

    teclas de atalho

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 import Grid from '../template/grid';
 import IconButton from '../template/iconButton';
 
-export default props => (
-    <div role="form" className="todoForm">
-        <Grid cols="12 9 10">
-            <input id="descricao" className="form-control" placeholder="Adicionar uma tarefa"
-                   value={props.descricao} onChange={props.funcaoMudanca}/>
-        </Grid>
-        <Grid cols="12 3 2">
-            <IconButton style="primary" icon="plus" onClick={props.funcaoAdd}/>
-            <IconButton style="info" icon="search" onClick={props.funcaoBusca}/>
-            <IconButton style="default" icon="close" onClick={props.funcaoLimpar}/>
-        </Grid>
-    </div>
-);
+export default props => {
+    const eventoTecla= (e) => {
+        if (e.key === 'Enter') e.shiftKey ? props.funcaoBusca() : props.funcaoAdd();
+        else if (e.key === 'Escape') props.funcaoLimpar();
+    };
+
+    return (
+        <div role="form" className="todoForm">
+            <Grid cols="12 9 10">
+                <input id="descricao" className="form-control" placeholder="Adicionar uma tarefa"
+                       onKeyUp={eventoTecla}
+                       value={props.descricao} onChange={props.funcaoMudanca}/>
+            </Grid>
+            <Grid cols="12 3 2">
+                <IconButton style="primary" icon="plus" onClick={props.funcaoAdd}/>
+                <IconButton style="info" icon="search" onClick={props.funcaoBusca}/>
+                <IconButton style="default" icon="close" onClick={props.funcaoLimpar}/>
+            </Grid>
+        </div>
+    );
+}
