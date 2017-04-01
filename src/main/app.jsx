26690485697bfd7c496a623674ae614f26ecd1b2
Jonathan Willian <jonathan.willian.tod@gmail.com>
2017-04-01 03:55:14 -0300

    Criação do Menu

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -4,9 +4,11 @@ import 'fontawesome_min_css';
 import React from 'react';
 import Todo from '../todo/todo';
 import Sobre from '../sobre/sobre';
+import Menu from '../template/menu';
 
 export default props => (
     <div clssName='container'>
+        <Menu/>
         <Todo/>
         <Sobre/>
     </div>
