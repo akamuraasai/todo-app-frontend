d140737969b9461956f4c73d3a3dfbdf5a1f919a
Jonathan Willian <jonathan.willian.tod@gmail.com>
2017-04-01 05:08:18 -0300

    finalizado o formulário de adição

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
+
 import PageHeader from '../template/pageHeader';
 import TodoForm from './todoForm';
 import TodoList from './todoList';
+
+const URL = 'http://localhost:3003/api/todos';
 
 export default class Todo extends Component {
     constructor(props) {
@@ -17,7 +21,9 @@ export default class Todo extends Component {
     }
 
     adicionaTarefa() {
-        console.log("funciona o add");
+        const descricao = this.state.descricao;
+        Axios.post(URL, { descricao })
+             .then(dados => console.log(dados));
     }
 
     render() {
